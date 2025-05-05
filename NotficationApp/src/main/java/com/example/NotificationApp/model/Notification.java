c32db8e23db8ce2545e950db2e481f3ad48cf7b6
WaleedElgamal <waleedelgamall3@gmail.com>
2025-05-06 00:32:10 +0300

    [add] Firebase messaging for push notifications

---
+++ NotficationApp/src/main/java/com/example/NotificationApp/model/Notification.java
@@ -0,0 +1,7 @@
+package com.example.NotificationApp.model;
+
+public class Notification {
+    // TODO Include attributes: senderID, receiverID, senderName, receiverName, receiverEmail,
+    //  type, timestamp, content
+
+}
