75bcb96e59a6f26e6fdb106caaf51a27efeded44
WaleedElgamal <waleedelgamall3@gmail.com>
2025-05-14 00:17:52 +0300

    [fix] typo fix

--- NotficationApp/src/main/java/com/example/NotificationApp/strategy/PushNotificationHandler.java
+++ NotficationApp/src/main/java/com/example/NotificationApp/strategy/PushNotificationHandler.java
@@ -2,13 +2,14 @@ package com.example.NotificationApp.strategy;
 
 import com.example.NotificationApp.model.Notification;
 import com.example.NotificationApp.service.PushNotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PushNotificationHandler implements NotificationHandler {
     private final PushNotificationService pushNotificationService;
 
-    @Au
+    @Autowired
     public PushNotificationHandler(PushNotificationService pushNotificationService) {
         this.pushNotificationService = pushNotificationService;
     }
