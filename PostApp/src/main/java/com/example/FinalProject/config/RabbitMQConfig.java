26cdefe0dc9665b2de748919398151c8e3414254
Yousif Sanad <yousif12223@yahoo.com>
2025-05-15 17:31:12 +0300

    checked the k8s folder

--- PostApp/src/main/java/com/example/FinalProject/config/RabbitMQConfig.java
+++ PostApp/src/main/java/com/example/FinalProject/config/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package com.example.FinalProject.config;
 
+import com.example.FinalProject.events.CommentEventPublisher;
+import com.example.FinalProject.events.LikeEventPublisher;
+import com.example.FinalProject.events.NotificationEventPublisher;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +12,13 @@ import org.springframework.amqp.core.Queue;
 
 @Configuration
 public class RabbitMQConfig {
+
+    public static final String USER_EVENTS_EXCHANGE = "user.events.exchange";
+    public static final String USER_DELETED_ROUTING_KEY = "user.event.deleted";
+    public static final String USER_BANNED_ROUTING_KEY = "user.event.banned";
+    public static final String USER_EVENTS_QUEUE = "user.events.queue";
+
+
     public static final String POST_EVENTS_EXCHANGE = "post.events.exchange";
     public static final String POST_CREATED_ROUTING_KEY = "post.event.created";
     public static final String POST_UPDATED_ROUTING_KEY = "post.event.updated";
@@ -52,5 +62,23 @@ public class RabbitMQConfig {
                 .to(sharedExchange)
                 .with(NOTIFICATION_ROUTING);
     }
+
+
+    @Bean
+    public TopicExchange notificationEventsExchange() {
+        return new TopicExchange(NotificationEventPublisher.NOTIFICATION_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange commentEventsExchange() {
+        return new TopicExchange(CommentEventPublisher.COMMENT_EVENTS_EXCHANGE);
+    }
+
+    @Bean
+    public TopicExchange likeEventsExchange() {
+        return new TopicExchange(LikeEventPublisher.LIKE_EVENTS_EXCHANGE);
+    }
+
+
 
 }
