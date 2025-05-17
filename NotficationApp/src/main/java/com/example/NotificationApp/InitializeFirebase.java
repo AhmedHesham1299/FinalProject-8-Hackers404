cd0a1129a95c007c4efdbc207a35a81c08281c20
YahiaEid <yahiae54@gmail.com>
2025-05-17 18:23:04 +0300

    fixed JSON converter in post app

--- NotficationApp/src/main/java/com/example/NotificationApp/InitializeFirebase.java
+++ NotficationApp/src/main/java/com/example/NotificationApp/InitializeFirebase.java
@@ -1,32 +1,32 @@
-package com.example.NotificationApp;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-
-@Component
-public class InitializeFirebase {
-
-    @Value("${firebase.config.path}")
-    private String firebaseConfigPath;
-
-
-    @PostConstruct
-    public void init() throws IOException {
-        if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-        }
-    }
-}
+//package com.example.NotificationApp;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//@Component
+//public class InitializeFirebase {
+//
+//    @Value("${firebase.config.path}")
+//    private String firebaseConfigPath;
+//
+//
+//    @PostConstruct
+//    public void init() throws IOException {
+//        if (FirebaseApp.getApps().isEmpty()) {
+//            FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
+//
+//            FirebaseOptions options = FirebaseOptions.builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//
+//            FirebaseApp.initializeApp(options);
+//        }
+//    }
+//}
