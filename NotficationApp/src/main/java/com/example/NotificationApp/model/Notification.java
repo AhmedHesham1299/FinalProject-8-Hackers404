287726ac38b45175fe4408c3d8733aaadbe4dba7
AhmedHesham1299 <127687214+AhmedHesham1299@users.noreply.github.com>
2025-05-17 17:02:51 +0300

    Bug fix

--- NotficationApp/src/main/java/com/example/NotificationApp/model/Notification.java
+++ NotficationApp/src/main/java/com/example/NotificationApp/model/Notification.java
@@ -21,7 +21,12 @@ public class Notification {
     @Id
     private String id;
     private String postID;
+
+
+
     private String senderID;
+
+
     private String receiverID;
     private String senderName;
     private String receiverName;
@@ -30,4 +35,109 @@ public class Notification {
     private String type;
     private LocalDateTime timestamp;
     private boolean read;
+
+
+    public Notification(String postID, String senderID, String receiverID, String senderName, String receiverName, String receiverEmail, String content, String type, LocalDateTime timestamp, boolean read) {
+        this.postID = postID;
+        this.senderID = senderID;
+        this.receiverID = receiverID;
+        this.senderName = senderName;
+        this.receiverName = receiverName;
+        this.receiverEmail = receiverEmail;
+        this.content = content;
+        this.type = type;
+        this.timestamp = timestamp;
+        this.read = read;
+    }
+
+
+
+    public String getReceiverID() {
+        return receiverID;
+    }
+
+    public void setReceiverID(String receiverID) {
+        this.receiverID = receiverID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPostID() {
+        return postID;
+    }
+
+    public void setPostID(String postID) {
+        this.postID = postID;
+    }
+
+    public String getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(String senderID) {
+        this.senderID = senderID;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getReceiverEmail() {
+        return receiverEmail;
+    }
+
+    public void setReceiverEmail(String receiverEmail) {
+        this.receiverEmail = receiverEmail;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
 }
--- NotficationApp/src/main/java/com/example/NotificationApp/model/NotificationPreferences.java
+++ NotficationApp/src/main/java/com/example/NotificationApp/model/NotificationPreferences.java
@@ -8,8 +8,31 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class NotificationPreferences {
     private boolean isEmailEnabled;
+
+
+
     private boolean isPushEnabled;
+
+    public NotificationPreferences(boolean isEmailEnabled, boolean isPushEnabled) {
+        this.isEmailEnabled = isEmailEnabled;
+        this.isPushEnabled = isPushEnabled;
+    }
+
+    public boolean isPushEnabled() {
+        return isPushEnabled;
+    }
+
+    public void setPushEnabled(boolean pushEnabled) {
+        isPushEnabled = pushEnabled;
+    }
+
+    public boolean isEmailEnabled() {
+        return isEmailEnabled;
+    }
+
+    public void setEmailEnabled(boolean emailEnabled) {
+        isEmailEnabled = emailEnabled;
+    }
 }
